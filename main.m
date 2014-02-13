50a5a99263838690b48b2f3029840e6b7edd641e
Enrico Maria Caporossi <enrico.caporossi@gmail.com>
2014-02-13 05:56:58 +0100

    ko

---
+++ main.m
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+
+ 
+
+int main(int argc, char *argv[])
+
+{
+
+    @autoreleasepool {
+
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+
+    }
+
+}
+
+@end
