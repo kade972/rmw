80154c27f906b40d6b99eb4326f7afa54c6c0991
Enrico Maria Caporossi <enrico.caporossi@gmail.com>
2014-02-13 19:33:55 +0100

    li m...

--- main.m
+++ main.m
@@ -48,11 +48,7 @@ Copyright (C) 2009 Apple Inc. All Rights Reserved.
 
 #import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[]) {
-
-NativeApplication.nativeApplication.executeInBackground = true;     
-
-    
+int main(int argc, char *argv[]) {    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
