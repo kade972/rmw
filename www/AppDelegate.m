573cda4334995bb32dd4c059689c50e9bdac3639
Enrico Maria Caporossi <enrico.caporossi@gmail.com>
2014-02-20 21:18:27 +0100

    classe

--- www/AppDelegate.m
+++ www/AppDelegate.m
@@ -7,6 +7,18 @@
 
     application.idleTimerDisabled = YES;
 
+Class playingInfoCenter = NSClassFromString(@"MPNowPlayingInfoCenter");
+
+if (playingInfoCenter) {
+    MPNowPlayingInfoCenter *center = [MPNowPlayingInfoCenter defaultCenter];
+    NSDictionary *songInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+                             @"Some artist", MPMediaItemPropertyArtist,
+                             @"Some title", MPMediaItemPropertyTitle,
+                             @"Some Album", MPMediaItemPropertyAlbumTitle,
+                             nil];
+    center.nowPlayingInfo = songInfo;
+}
+
     // Override point for customization after app launch    
    }
 
