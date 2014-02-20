d3c8c8e05fc941e3f84e463b05867664ef4b0d9d
Enrico Maria Caporossi <enrico.caporossi@gmail.com>
2014-02-20 22:41:51 +0100

    background

--- www/AppDelegate.m
+++ www/AppDelegate.m
@@ -7,15 +7,33 @@
 
     application.idleTimerDisabled = YES;
 
-[[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
 
-
-NSError *setCategoryErr = nil;
- NSError *activationErr  = nil;
- [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error:&setCategoryErr];
- [[AVAudioSession sharedInstance] setActive:YES error:&activationErr];
-    // Override point for customization after app launch    
    }
+
+
+
+-(void) initAudioSession {
+// Registers this class as the delegate of the audio session to listen for audio interruptions
+[[AVAudioSession sharedInstance] setDelegate: self];
+//Set the audio category of this app to playback.
+NSError *setCategoryError = nil; [[AVAudioSession sharedInstance]
+  setCategory: AVAudioSessionCategoryPlayback error: &setCategoryError];
+if (setCategoryError) {
+//RESPOND APPROPRIATELY
+}
+
+// Registers the audio route change listener callback function
+// An instance of the audio player/manager is passed to the listener
+AudioSessionAddPropertyListener ( kAudioSessionProperty_AudioRouteChange,
+audioRouteChangeListenerCallback, self );
+
+//Activate the audio session
+NSError *activationError = nil;
+[[AVAudioSession sharedInstance] setActive: YES error: &activationError];
+if (activationError) {
+//RESPOND APPROPRIATELY
+}
+}
 
 
 - (void)dealloc {
