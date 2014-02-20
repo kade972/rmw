c35d3103ec492241c9a41aaae3e7933914a704eb
Enrico Maria Caporossi <enrico.caporossi@gmail.com>
2014-02-20 23:32:34 +0100

    avfundation

--- www/AppDelegate.m
+++ www/AppDelegate.m
@@ -4,16 +4,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-
     AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    BOOL ok;
-    NSError *setCategoryError = nil;
-    ok = [audioSession setCategory:AVAudioSessionCategoryPlayback
-                             error:&setCategoryError];
-    
-
+BOOL ok;
+NSError *setCategoryError = nil;
+ok = [audioSession setCategory:AVAudioSessionCategoryPlayback
+                         error:&setCategoryError];
 
     return YES;
 }
