86fb6beb444dfe5894bbb9b2fa9c832c2ebbfd5f
Enrico Maria Caporossi <enrico.caporossi@gmail.com>
2014-02-20 22:53:18 +0100

    aggiunto file esterno

--- www/AppDelegate.m
+++ www/AppDelegate.m
@@ -1,43 +1,39 @@
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 @implementation AppDelegate
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    BOOL ok;
+    NSError *setCategoryError = nil;
+    ok = [audioSession setCategory:AVAudioSessionCategoryPlayback
+                             error:&setCategoryError];
+    if (!ok) {
+        NSLog(@"%s setCategoryError=%@", __PRETTY_FUNCTION__, setCategoryError);
+    }
 
 
-- (void)applicationDidFinishLaunching:(UIApplication *)application {    
-
-    application.idleTimerDisabled = YES;
-
-
-   }
+    UIViewController *vc = [[UIViewController alloc] init];
+    UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController: vc];
+    self.window.rootViewController = nav;
 
 
-
--(void) initAudioSession {
-// Registers this class as the delegate of the audio session to listen for audio interruptions
-[[AVAudioSession sharedInstance] setDelegate: self];
-//Set the audio category of this app to playback.
-NSError *setCategoryError = nil; [[AVAudioSession sharedInstance]
-  setCategory: AVAudioSessionCategoryPlayback error: &setCategoryError];
-if (setCategoryError) {
-//RESPOND APPROPRIATELY
-}
-
-// Registers the audio route change listener callback function
-// An instance of the audio player/manager is passed to the listener
-AudioSessionAddPropertyListener ( kAudioSessionProperty_AudioRouteChange,
-audioRouteChangeListenerCallback, self );
-
-//Activate the audio session
-NSError *activationError = nil;
-[[AVAudioSession sharedInstance] setActive: YES error: &activationError];
-if (activationError) {
-//RESPOND APPROPRIATELY
-}
-}
+    //WEBVIEW
+    UIWebView *myWeb = [[UIWebView alloc] initWithFrame:CGRectMake(0, 200, 320, vc.view.frame.size.height)];
+    myWeb.mediaPlaybackRequiresUserAction = NO;
+    myWeb.allowsInlineMediaPlayback = YES;
+    [vc.view addSubview:myWeb];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL: [NSURL URLWithString: @"http://www.radiomedweb.com/audio.html"]];
+    [myWeb loadRequest: request];
 
 
-- (void)dealloc {
-    [super dealloc];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
 }
 
 
